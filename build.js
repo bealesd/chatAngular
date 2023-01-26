468d697c3102a41825e41e3d5e388d152f65a25f
David Beales <David.Beales@ukho.gov.uk>
2023-01-26 09:20:50 +0000

    update actions

--- build.js
+++ build.js
@@ -1,17 +1,20 @@
 const fs = require("fs");
 const path = require('path');
 const { execSync } = require('child_process');
+let core;
 
 try {
-    const core = require("@actions/core");
+    //if running in github action us core for error messaging
+    core = require("@actions/core");
 }
 catch {
-    const core = { setFailed: (msg) => { console.log(msg); } };
+    //if running locally print error to console
+    core = { setFailed: (msg) => { console.log(msg); } };
 }
 
 class Build {
     outputDir = 'docs';
-    buildCommand = `ng build --prod --output-path docs --base-href "/chatAngular/"`;
+    buildCommand = `ng build --configuration production --output-path docs --base-href "/chatAngular/"`;
 
     constructor() {
         this.fiveMinuteTimeout = 1000 * 60 * 5;
@@ -19,14 +22,17 @@ class Build {
 
     main() {
         try {
-            fs.existsSync(this.outputDir)
-            fs.rmdirSync(this.outputDir, { recursive: true });
-
+            //clean static web files by removing docs directory
+            if (fs.existsSync(this.outputDir))
+                fs.rmdirSync(this.outputDir, { recursive: true });
+            
+            //generate static web files in doc directory
             execSync(this.buildCommand, { timeout: this.fiveMinuteTimeout, stdio: 'inherit' });
 
+            //create a 404 file so uknown routes can be resolved
             fs.copyFileSync(path.join(this.outputDir, 'index.html'), path.join(this.outputDir, '404.html'));
         } catch (e) {
-            core.setFailed(e.message);
+            core.setFailed(`Error in build.js, generating static web files.\nError: ${e.message}.`);
         } finally { }
     }
 }
